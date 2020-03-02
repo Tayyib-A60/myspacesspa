4e91d7f5ede95863115527c33e0f9550781dd12f
Toyeeb Adesokan <tadesokan@appzonegroup.com>
2020-03-02 10:01:00 +0100

    changed made

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CLIENT'));
+app.use(express.static('./dist/client'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname,'/dist/CLIENT/index.html'));
+    res.sendFile(path.join(__dirname,'./../dist/client/index.html'));
 
 });
 
